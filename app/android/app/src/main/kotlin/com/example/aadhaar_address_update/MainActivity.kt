7da4f611588d23fae7e25b91eeaf5787e948a800
ankithans <ankithans1947@gmail.com>
2021-10-25 18:41:18 +0530

    chore: initialize flutter app

---
+++ app/android/app/src/main/kotlin/com/example/aadhaar_address_update/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aadhaar_address_update
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:aadhaar_address_update/presentation/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Address Update',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}

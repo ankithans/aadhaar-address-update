11715e7c8bcd641b5e2dac993fd23d2c055c5178
ankithans <ankithans1947@gmail.com>
2021-10-27 16:39:08 +0530

    chore: switch main to login screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LandlordHomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }

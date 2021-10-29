7bcad843a120459de55799d2fea2c4416cf50976
ankithans <ankithans1947@gmail.com>
2021-10-30 01:40:58 +0530

    feat: new login schema integrated

--- app/lib/data/models/tenant/tenant_login.dart
+++ app/lib/data/models/tenant/tenant_login.dart
@@ -27,13 +27,13 @@ class Data {
     required this.phone,
   });
   late final String id;
-  late final String address;
+  late final Address1 address;
   late final String fcm;
   late final int phone;
 
   Data.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    address = json['address'];
+    address = Address1.fromJson(json['address']);
     fcm = json['fcm'];
     phone = json['phone'];
   }
@@ -41,9 +41,58 @@ class Data {
   Map<String, dynamic> toJson() {
     final _data = <String, dynamic>{};
     _data['id'] = id;
-    _data['address'] = address;
+    _data['address'] = address.toJson();
     _data['fcm'] = fcm;
     _data['phone'] = phone;
+    return _data;
+  }
+}
+
+class Address1 {
+  Address1({
+    required this.co,
+    required this.country,
+    required this.dist,
+    required this.house,
+    required this.lm,
+    required this.loc,
+    required this.pc,
+    required this.state,
+    required this.vtc,
+  });
+  late final String co;
+  late final String country;
+  late final String dist;
+  late final String house;
+  late final String lm;
+  late final String loc;
+  late final String pc;
+  late final String state;
+  late final String vtc;
+
+  Address1.fromJson(Map<String, dynamic> json) {
+    co = json['co'];
+    country = json['country'];
+    dist = json['dist'];
+    house = json['house'];
+    lm = json['lm'];
+    loc = json['loc'];
+    pc = json['pc'];
+    state = json['state'];
+    vtc = json['vtc'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['co'] = co;
+    _data['country'] = country;
+    _data['dist'] = dist;
+    _data['house'] = house;
+    _data['lm'] = lm;
+    _data['loc'] = loc;
+    _data['pc'] = pc;
+    _data['state'] = state;
+    _data['vtc'] = vtc;
     return _data;
   }
 }
